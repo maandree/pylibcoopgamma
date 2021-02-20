494e239f7597b5f279395776f5375c7fb2cea288
Mattias Andrée <maandree@kth.se>
2021-02-20 16:02:55 +0100

    m improvement to config.mk
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 PREFIX = /usr
 
-PY_MAJOR   = $$($(PYTHON) --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 1)
-PY_MINOR   = $$($(PYTHON) --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 2)
+PY_MAJOR   = $$(python --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 1)
+PY_MINOR   = $$(python$(PYTHON_MAJOR) --version 2>&1)
 PY_VER     = $(PY_MAJOR)$(PY_MINOR)
 PY_VERSION = $(PY_MAJOR).$(PY_MINOR)
 
@@ -9,7 +9,7 @@ PYTHONDIR = $(PREFIX)/lib/python$(PY_VERSION)
 
 CYTHON    = cython
 PKGCONFIG = pkg-config
-PYTHON    = python3
+PYTHON    = python$(PY_MAJOR)
 
 CPPFLAGS =
 CFLAGS   = -std=c99 -Wall $$($(PKGCONFIG) --cflags python$(PY_MAJOR)) -O2
